8fa24fc97e57a9d27d56524d15d012644ea2b525
xavie <xavie@XAVIERLAP>
2025-01-18 22:37:01 -0500

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDBC.cs
@@ -0,0 +1,16 @@
+﻿using APIProductos.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace APIProductos.Data
+{
+    public class ApplicationDBC:DbContext
+    {
+        public ApplicationDBC(
+            DbContextOptions<ApplicationDBC> options): base(options) 
+        {
+
+        }
+        DbSet<Producto> Producto{get; set;}
+    }
+}
